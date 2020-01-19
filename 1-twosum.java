de56f5b0e756584f09eb43c4bb456a4f851cb604
Frank Zhang <frankzhang@fortinet.com>
2020-01-19 13:35:40 -0800

    01/17/2020

---
+++ 1-twosum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums.length < 3) {
+            return new int[]{0, 1};
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+        return new int[0];
+    }
+}
